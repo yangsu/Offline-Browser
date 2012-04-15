dfe9cf243912e13f25026ae707b31bc8c6ff2735
Alex Galonsky <alex@galonsky.com>
2012-04-14 23:41:32 -0400

    Storing data on a per-tab basis. Deleting data on tab close

--- src/js/background.js
+++ src/js/background.js
@@ -1,39 +1,44 @@
 var globaldatastore = {};
-$(document).ready(function () {
+$(document).ready(function() {
   console.log('Hello from Background Script');
 
   // Send request to current tab
-  sendRequest = function (data) {
-    chrome.tabs.getSelected(null, function (tab) {
+  sendRequest = function(data) {
+    chrome.tabs.getSelected(null, function(tab) {
       chrome.tabs.sendRequest(
-        tab.id,
-        { message: data },
-        function (response) {
-          console.log(response.message);
-        }
-      );
+      tab.id, {
+        message: data
+      }, function(response) {
+        console.log(response.message);
+      });
     });
   };
 
-  chrome.extension.onRequest.addListener(function (request, sender, sendResponse) {
-    console.log(sender.tab ?
-                'from a content script:' + sender.tab.url :
-                'from the extension');
+  chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
+    console.log(sender.tab ? 'from a content script:' + sender.tab.url : 'from the extension');
     console.log(request.message);
     var links = request.message,
       i, l, url;
+
+    globaldatastore[sender.tab.id] = {};
     for (i = 0, l = links.length; i < l; i += 1) {
       url = links[i];
       if (url.indexOf('http://') === 0 || url.indexOf('https://') === 0) {
-        cacheURL(url);
+        cacheURL(sender.tab.id, url);
       }
     }
-    sendResponse({ message: request.message });
+    sendResponse({
+      message: request.message
+    });
   });
 });
 
-function cacheURL(url) {
-  $.get(url, function (data) {
-    globaldatastore[url] = data;
+function cacheURL(tabid, url) {
+  $.get(url, function(data) {
+    globaldatastore[tabid][url] = data;
   });
 }
+
+chrome.tabs.onRemoved.addListener(function(tabId, removeInfo) {
+  delete globaldatastore[tabId];
+});
