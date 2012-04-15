dbb316f0f8e179bb68853615464ac5ad934b957b
Yang Su <suyang1+services@gmail.com>
2012-04-14 22:23:51 -0400

    preliminary implementations of request/response between extension and contentscript

--- src/js/background.js
+++ src/js/background.js
@@ -1,5 +1,24 @@
-document.onreadystatechange = function () {
-  if (document.readyState == "complete") {
-    console.log('Hello from Background Script');
-  }
-};
+$(document).ready(function () {
+  console.log('Hello from Background Script');
+
+  // Send request to current tab
+  sendRequest = function (data) {
+    chrome.tabs.getSelected(null, function (tab) {
+      chrome.tabs.sendRequest(
+        tab.id,
+        { message: data },
+        function (response) {
+          console.log(response.message);
+        }
+      );
+    });
+  };
+
+  chrome.extension.onRequest.addListener(function (request, sender, sendResponse) {
+    console.log(sender.tab ?
+                'from a content script:' + sender.tab.url :
+                'from the extension');
+    console.log(request.message);
+    sendResponse({ message: request.message });
+  });
+});
--- src/js/contentScript.js
+++ src/js/contentScript.js
@@ -1,3 +1,19 @@
 $(document).ready(function () {
   console.log('Hello from Content Script');
+  sendRequest = function (data) {
+    chrome.extension.sendRequest(
+      { message: data },
+      function (response) {
+        console.log(response.message);
+      }
+    );
+  };
+  chrome.extension.onRequest.addListener(function (request, sender, sendResponse) {
+    console.log(sender.tab ?
+                'from a content script:' + sender.tab.url :
+                'from the extension');
+    console.log(request.message);
+    sendResponse({ message: request.message });
+  });
 });
+var aaa = '1';
