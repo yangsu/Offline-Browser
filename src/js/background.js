5e772891c449b86ee678ca1cd7d4040251eb450c
Alex Galonsky <alex@galonsky.com>
2012-04-14 22:26:00 -0400

    handleDOM function to get all the links

--- src/js/background.js
+++ src/js/background.js
@@ -4,12 +4,23 @@ document.onreadystatechange = function() {
     }
 };
 
+function handleDOM(dom) {
+    links = dom.getElementsByTagName('a');
+    for(var i = 0; i < links.length; i++) {
+        url = links[i].getAttribute('href');
+        if(url.indexOf('http://') == 0 || url.indexOf('https://')) {
+            cacheURL(url);
+        }
+    }
+}
+
 function cacheURL(url) {
     var xhr = new XMLHttpRequest();
     xhr.open("GET", url, true);
     xhr.onreadystatechange = function() {
         if (xhr.readyState == 4) {
             data = xhr.responseText;
+            localStorage[url] = data;
         }
     }
     xhr.send();
