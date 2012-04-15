e150226cc647b4647c69d97c2711d81742128cfc
Alex Galonsky <alex@galonsky.com>
2012-04-14 22:00:20 -0400

    Simple cacheURL function

--- src/js/background.js
+++ src/js/background.js
@@ -1,5 +1,16 @@
-document.onreadystatechange = function () {
-  if (document.readyState == "complete") {
-    console.log('Hello from Background Script');
-  }
+document.onreadystatechange = function() {
+    if (document.readyState == "complete") {
+        console.log('Hello from Background Script');
+    }
 };
+
+function cacheURL(url) {
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", url, true);
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState == 4) {
+            data = xhr.responseText;
+        }
+    }
+    xhr.send();
+}
