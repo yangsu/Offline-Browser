20f7bad6f8ffe2245e3700ba6e0255c70540cc33
Alex Galonsky <alex@galonsky.com>
2012-04-15 17:05:46 -0400

    Only overriding links when we're offline. We can send an offline message from background to contentScript later.

--- src/js/contentScript.js
+++ src/js/contentScript.js
@@ -1,3 +1,5 @@
+var offline = false;
+
 $(document).ready(function () {
   console.log('Hello from Content Script');
   sendRequest = function (data, callback) {
@@ -22,42 +24,44 @@ $(document).ready(function () {
     links.anchors.push(link.href);
   })
   .live('click', function (event) {
-    var url = event.target.href;
-    sendRequest({
-      type: 'url',
-      data: url
-    }, function (response) {
-      var data = response.data;
-      if (data) {
-        if (data.type === 'html') {
-          document.write(data.data);
-          $('img', document).each(function (i, img) {
-            sendRequest({
-              type: 'url',
-              data: img.src
-            }, function (response) {
-              img.src = response.data;
-            });
-          });
-          $('link', document).each(function (i, linktag) {
-            sendRequest({
-              type: 'url',
-              data: linktag.href
-            }, function (response) {
-              $('<style type="text/css">' + response.data + '</style>').appendTo('head');
-              $(this).remove();
-            });
-          });
-        }
-        else if (data.type === 'image') {
-          document.write('<html><body></body></html>');
-          var i = new Image();
-          i.src = data.data;
-          document.body.appendChild(i);
-        }
+    if(offline){
+        var url = event.target.href;
+        sendRequest({
+          type: 'url',
+          data: url
+        }, function (response) {
+          var data = response.data;
+          if (data) {
+            if (data.type === 'html') {
+              document.write(data.data);
+              $('img', document).each(function (i, img) {
+                sendRequest({
+                  type: 'url',
+                  data: img.src
+                }, function (response) {
+                  img.src = response.data;
+                });
+              });
+              $('link', document).each(function (i, linktag) {
+                sendRequest({
+                  type: 'url',
+                  data: linktag.href
+                }, function (response) {
+                  $('<style type="text/css">' + response.data + '</style>').appendTo('head');
+                  $(this).remove();
+                });
+              });
+            }
+            else if (data.type === 'image') {
+              document.write('<html><body></body></html>');
+              var i = new Image();
+              i.src = data.data;
+              document.body.appendChild(i);
+            }
+          }
+        });
+        event.preventDefault();
       }
-    });
-    event.preventDefault();
   });
 
   // Save all images
